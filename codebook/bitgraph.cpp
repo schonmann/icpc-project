2e1913de233700a6b372e4ae94b4565b37a37edb
Antonio Schönmann <antonio_schonmann@hotmail.com>
2017-08-10 01:53:45 -0300

    Uncomment.

--- codebook/bitgraph.cpp
+++ codebook/bitgraph.cpp
@@ -65,22 +65,13 @@ class BitGraph {
 		}
 		bool HasEdge(int u, int v) {
 			this->assertValidEdge(u,v);
-
 			BitwiseIndex tv = BitwiseIndex(v);
-
 			return this->Graph[u][tv.getIdx()] & tv.getOffset();
 		}
 		void AddEdge(int u, int v) {
 			this->assertValidEdge(u,v);
-
-			/* Traduz indice da coluna para indice dentro do byte. */
-
 			BitwiseIndex tv = BitwiseIndex(v);
-
-			/* Faz bitwise OR com o offset, adicionando '1' na posicao correspondente do byte. */
-
 			this->Graph[u][tv.getIdx()] |= tv.getOffset();
-
 			if (!this->directed) {
 				BitwiseIndex tu = BitwiseIndex(u);
 				this->Graph[v][tu.getIdx()] |= tu.getOffset();				
@@ -88,23 +79,15 @@ class BitGraph {
 		}
 		void RemoveEdge(int u, int v) {
 			this->assertValidEdge(u,v);
-
-			/* Traduz indice da coluna para indice dentro do byte. */
-
 			BitwiseIndex tv = BitwiseIndex(v);
-
-			/* Faz bitwise AND com o complemento do offset, removendo o '1' da posicao correspondente no byte */
-
 			this->Graph[u][tv.getIdx()] &= ~tv.getOffset();
-
 			if (!this->directed) {
 				BitwiseIndex tu = BitwiseIndex(u);
 				this->Graph[v][tu.getIdx()] &= ~tu.getOffset();				
 			}
 		}
-
 		void PrintGraph() {
-			cout << (this->directed ? "Directed " : "") << "Graph: " << endl << endl;
+			cout << (this->directed ? "Directed" : "Undirected") << " Graph: " << endl << endl;
 			for(int i = 0; i < this->n; i++) {
 				for(int j = 0; j < this->n; j++) {
 					cout << this->HasEdge(i,j) << " ";
@@ -116,28 +99,29 @@ class BitGraph {
 };
 
 int main(void) {
-	BitGraph Graph(9);
-
-	Graph.AddEdge(0,0);
-	Graph.AddEdge(0,1);
-	Graph.AddEdge(0,2);
-
-	Graph.PrintGraph();
 	
-	BitGraph DirectedGraph(9, true);
+	BitGraph UndirectedGraph(9, false);
+	
+	UndirectedGraph.AddEdge(0,0);
+	UndirectedGraph.AddEdge(0,1);
+	UndirectedGraph.AddEdge(0,2);
+	
+	UndirectedGraph.PrintGraph();
 
+	BitGraph DirectedGraph(9, true);
+	
 	DirectedGraph.AddEdge(0,0);
 	DirectedGraph.AddEdge(0,1);
 	DirectedGraph.AddEdge(0,2);
-
+	
 	DirectedGraph.PrintGraph();
 
 	return 0;
 }
 
-/* Output */:
+/* Output */
 
-// 	Graph:
+// 	Undirected Graph:
 //
 // 	1 1 1 0 0 0 0 0 0
 // 	1 0 0 0 0 0 0 0 0
