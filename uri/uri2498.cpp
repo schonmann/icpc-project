2411c6480d1ac0ba721578733aeda8e56a828c57
Yago Tomé <yago.tome@hotmail.com>
2017-08-27 23:00:15 -0300

    Optimizing Knapsack Algorithm

--- uri/uri2498.cpp
+++ uri/uri2498.cpp
@@ -7,18 +7,18 @@ int main() {
 	int N, W, caseNum = 0;
 	while (1) {
 		cin >> N >> W;
-		int T[N+1][W+1];
+		int T[W+1];
 		memset(T, 0, sizeof T);
 		for (int i = 1; i <= N; i++) {
 			int w, v;
 			cin >> w >> v;
-			for (int j = 1; j <= W; j++)
-				T[i][j] = j < w ? T[i-1][j] : max(T[i-1][j-w] + v, T[i-1][j]);
+			for (int j = W; j >= w; j--)
+				T[j] = max(T[j-w] + v, T[j]);
 		}
 		int g = -1;
 		for (int j = 0; j <= W; j++)
-			if (T[N][j] > g)
-				g = T[N][j];
+			if (T[j] > g)
+				g = T[j];
 		if (!(N || W))
 			break;
 		printf("Caso %d: %d\n", ++caseNum, g);
