c94fcb77b8f6fe4019edd1fbb386d7bad190f131
Xuma <antonio_schonmann@hotmail.com>
2018-12-08 23:33:24 -0800

    PCSDEVO

---
+++ codebook/dynprog/pcsdevo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+typedef int T;
+
+int numPairsWithSum(vector<T> &elements, int sum) {
+    map<T,int> m;
+    for(int i = 0; i < elements.size(); i++) {
+        m[elements[i]]++;
+    }
+    int count = 0;
+    for(int i = 0; i < elements.size(); i++) {
+        count += m[sum-elements[i]];
+        if(sum - elements[i] == elements[i]) {
+            count--;
+        }
+    }
+    return count/2;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int s, n; cin >> s >> n; 
+    vector<T> elements(n);
+
+    for(int i = 0; i < elements.size(); i++) {
+        T x; cin >> x; elements[i] = x;
+    }
+
+    cout << numPairsWithSum(elements, s) << endl;
+}
