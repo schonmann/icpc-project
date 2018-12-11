2a5e03ba6e4699badba7426f4711eed4fa902e46
Xuma <antonio_schonmann@hotmail.com>
2018-12-10 20:53:25 -0800

    Formatting

--- codebook/graphs/transitive_closure.cpp
+++ codebook/graphs/transitive_closure.cpp
@@ -39,10 +39,10 @@ void printSolution(vector<vector<int>> reach) {
 }
 
 int main() {
-	int n; cin >> n; 
-    
-    vector<vector<int>> graph(n, vector<int>(n, 0));
+    int n; cin >> n;
 
+    vector<vector<int>> graph(n, vector<int>(n, 0));
+    
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++) cin >> graph[i][j];
 
