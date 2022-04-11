7074b3c7648cf2fc3978457d422ea58a0f5eb66a
Antonio Schönmann <antonio_schonmann@hotmail.com>
2022-04-11 10:00:37 -0300

    feature(longest-substring-...): add C++ solution

---
+++ leetcode/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> indexByChar;
+        
+        int subStringLength = 0;
+        int bestSubStringLength = 0;
+        int startingSubStringIndex = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            char currentChar = s[i];
+
+            map<char,int>::iterator duplicateCharIterator = indexByChar.find(currentChar);
+            bool substringHasDuplicateChar = duplicateCharIterator != indexByChar.end() &&
+                duplicateCharIterator->second >= startingSubStringIndex;
+
+            if (substringHasDuplicateChar) {
+                bestSubStringLength = max(bestSubStringLength, subStringLength);
+                startingSubStringIndex = duplicateCharIterator->second;
+                subStringLength = i - startingSubStringIndex - 1;
+                indexByChar.erase(currentChar);
+            }
+            
+            subStringLength++;
+            indexByChar[currentChar] = i;
+        }
+
+        return max(subStringLength, bestSubStringLength);
+    }
+};
